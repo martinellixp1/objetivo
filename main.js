ae218587ca311cfbfd53d57839860091d277e16b
martinellixp1 <miguel.martinelli@escola.pr.gov.br>
2026-05-12 07:54:47 -0300

    .

--- main.js
+++ main.js
@@ -2,3 +2,15 @@ const botoes = document.querySelectorAll(".botao")
 console.log(botoes)
 const textos = document.querySelectorAll(".aba-conteudo")
 console.log(textos)
+
+for (let i = 0; i < botoes.length; i++) {
+    botoes[1].onclick = function(){
+        for(let j=0; j<botoes; j++){
+            botoes[j].classList.remove("ativo")
+            textos[j].classList.remove("ativo")
+        }
+        botoes[i].classList.add("ativo")
+            textos[i].classList.add("ativo")
+    }
+    
+}
